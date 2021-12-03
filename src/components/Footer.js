a5aa0c728adcaf24c069e814f982097e0094c9e6
cedricoco <55392666+Benjam-byte@users.noreply.github.com>
2021-12-03 10:52:31 +0100

    HUD2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,8 @@ function Footer() {
       <input className='btn1' type="button" value="Remonter" />
       <h2 className='viz1'>nom de la view</h2>
       <p className='viz2'>x : </p>
-      <p className='viz3'>y : </p>
+      <p className='viz3'>y : </p> 
+      <p>testUHD</p>
       <input className='btn2' type="button" value="Créer un sujet"/>
     </div>
   );
